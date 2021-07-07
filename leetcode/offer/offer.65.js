4332ee0852971dd6523728ea142d054e814b489e
xiaohuolong <xiaohuolong@uxin.com>
2021-07-07 22:58:27 +0800

    fix

--- leetcode/offer/offer.65.js
+++ leetcode/offer/offer.65.js
@@ -2,7 +2,7 @@
  * @Author: xiaohuolong
  * @Date: 2021-03-12 18:16:35
  * @LastEditors: xiaohuolong
- * @LastEditTime: 2021-03-12 18:20:49
+ * @LastEditTime: 2021-07-07 22:44:16
  * @FilePath: /js-demo/leetcode/offer/offer.65.js
  */
 /**
@@ -20,12 +20,57 @@
  */
 var add = function(a, b) {
     while(b != 0){
+        let c = a ^ b
+        let d = (a & b) << 1
+        a = c
+        b = d
         // 进位
-        let c = (a & b) << 1
-        a ^= b
-        b = c
+    //     let c = (a & b) << 1
+    //     a ^= b
+    //     b = c
     }
     return a
 };
 
-console.log(add(10, 29))
+var minus = function(a, b){
+    return add(a, -b)
+}
+
+var multiply = function(a, b){
+    let ans = 0
+    while(b) {
+        if(b & 1) {
+            ans = add(a, ans);
+        }
+        a = add(a, a);
+        b >>= 1;
+    }
+    return ans
+}
+
+var division = function(a, b){
+    let count = 0
+    while(a >= b){
+        a = minus(a, b)
+        count++
+    }
+    return count
+}
+
+var pow = function(a, b){
+    let ans = 1
+    while(b) {
+        if(b & 1) {
+            ans = multiply(ans, a);
+        }
+        a = multiply(a, a);
+        b >>= 1;
+    }
+    return ans
+}
+
+console.log(add(5, 6))
+console.log(minus(5, 106))
+console.log(multiply(5, 101))
+console.log(division(100, 5))
+console.log(pow(2, 10))
