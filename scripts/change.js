462a2a86776f1dda37886e93c140c79db71961da
Louyuda <562958029@qq.com>
2022-04-03 11:52:35 +0800

    fix

--- scripts/change.js
+++ scripts/change.js
@@ -6,6 +6,7 @@
  * @FilePath: /js-demo/scripts/change.js
  */
 const { exec } = require('shelljs')
+const { join } = require('path')
 const fs = require('fs')
 const chalk = require('chalk')
 // // 获取变更文件夹
@@ -14,20 +15,23 @@ const change = process.env.change || '';
 console.log(chalk.green('==== 执行change脚本 ===='))
 console.log(chalk.red(`==== 变更${change} ====`))
 if(change && change != 'scripts/change.js'){
-    if(change.includes('.test.')){
-        console.log(chalk.red(`==== 执行 jest ${change} ====`))
-        exec(`jest ${change}`)
+  if(change.includes('.test.')){
+    console.log(chalk.red(`==== 执行 jest ${change} ====`))
+    exec(`jest ${change}`)
+  }
+  else {
+    try {
+      // 基于window的改动
+      let path = `${join(__dirname.replace('scripts', ''), change.replace('.js', '.test.js'))}`
+      path = path.replace(/\\/ig, '/')
+      let stat = fs.statSync(path)
+      console.log(chalk.red(`==== 执行 jest ${path} ====`))
+      exec(`jest ${path}`)
+    } catch (error) {
+      console.log(error)
+      console.log(chalk.red(`==== 执行 node ${change} ====`))
+      exec(`node ${change}`)
     }
-    else {
-        try {
-            let path = `${change.replace('.js', '.test.js')}`
-            let stat = fs.statSync(path)
-            console.log(chalk.red(`==== 执行 jest ${path} ====`))
-            exec(`jest ${path}`)
-        } catch (error) {
-            console.log(chalk.red(`==== 执行 node ${change} ====`))
-            exec(`node ${change}`)
-        }
-    }
+  }
 }
 console.log(chalk.green(`==== 执行结束 ====`))
